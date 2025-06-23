43ceb9cd534e0280168a0c8cf6ac6357204e8de9
madhvikrana <149559023+madhvikrana@users.noreply.github.com>
2025-06-23 12:30:02 -0400

    commit1

---
+++ DataSizeRange.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+public class DataSizeRange {
+    public static void main(String args[])
+    {
+        System.out.println("Int Min:" +Integer.MIN_VALUE);
+        System.out.println("Int Max: "+ Integer.MAX_VALUE);
+        System.out.println("Int Bytes "+ Integer.BYTES);
+    }
+}
