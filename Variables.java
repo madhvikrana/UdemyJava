2b44e1f6adb8e334ec592adb0063f74fc6a7e356
madhvikrana <149559023+madhvikrana@users.noreply.github.com>
2025-06-23 13:11:29 -0400

    commit2

---
+++ Variables.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+public class Variables {
+    public static void main(String arg[])
+    {
+        // byte b=5;
+        // System.out.println(b);
+        byte b=35;
+        System.out.println(b);
+        short s=300;
+        int i=100;
+
+        float f=25.3f;
+        char c='A';
+
+        System.out.println(f);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(c);
+    }
+    
+}
