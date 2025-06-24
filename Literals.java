c089cfbc9d06e4775084ac38d3f0b046303ca3c7
madhvikrana <149559023+madhvikrana@users.noreply.github.com>
2025-06-24 15:14:04 -0400

    commit4

---
+++ Literals.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+public class Literals {
+        public static void main (String args[])
+
+        {
+            byte b =15;
+            System.out.println(b);
+            short s=15;
+            int i=15;
+            System.out.println(s);
+            System.out.println(i);
+
+        }
+    }
+    
+
