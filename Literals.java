525413f17e879c1e21c537ef6ae41643a7c34674
madhvikrana <149559023+madhvikrana@users.noreply.github.com>
2025-07-09 21:37:35 -0400

    commit 5

--- Literals.java
+++ Literals.java
@@ -9,7 +9,25 @@ public class Literals {
             int i=15;
             System.out.println(s);
             System.out.println(i);
+            byte b1 = 0b1010;
+            byte b2 = 10;
+            byte b3 = 012;
+            byte b4 = 0XA;
+            System.out.println(b1);
+            System.out.println(b2);
+            System.out.println(b3);
+            System.out.println(b4);
+            long l=12599999999999L;
+            System.out.println(l);
+            float f=12.56f;
+            double d=12.56d;
+            System.out.println(f);
+            System.out.println(d);
 
+            long l1 = 999_999_999; // we use _ to improve the readability of the program the _ doesnt come in the output.         
+            float f1 = 123_456.56f;
+        System.out.println(l1);
+    System.out.println(f1);
         }
     }
     
