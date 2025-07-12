feedb8bd7dd3fce463b1b96d7251e9d20311a19a
madhvikrana <149559023+madhvikrana@users.noreply.github.com>
2025-07-11 23:27:28 -0400

    commit6

---
+++ Integraldatatype.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class Integraldatatype {
+    public static void main(String[] args) {
+        //to check binary form
+        int x=-5;
+        System.out.println(Integer.toBinaryString(x));
+
+    }
+    
+}
