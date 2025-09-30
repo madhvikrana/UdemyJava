8c7505a0d1301265c8de9e5540f1eb8dc6ebdb01
madhvikrana <149559023+madhvikrana@users.noreply.github.com>
2025-09-30 12:08:21 -0400

    commit12

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,11 +1,22 @@
 public class Arithmetic {
     public static void main(String[] args)
     {
-        int a=14 , b=5;
-        int c=a/b;
-        int r=a%b;
-        System.out.println(c);
-        System.out.println(r);
+        // int a=14 , b=5;
+
+        // int c=a/b;
+        // int r=a%b;
+        // System.out.println(c);
+        // System.out.println(r);
+
+        float a=14.3f, b=3.2f;
+
+        // float c=(float)a/b;
+        // System.out.println(c);
+
+        float r=a%b;
+        System.out.println(r);        
+
+        
     }
     
 }
