e1630686bc79c565956b21ea45fc0878723b71d7
madhvikrana <149559023+madhvikrana@users.noreply.github.com>
2025-10-04 14:01:37 -0400

    commit13

--- Arithmetic.java
+++ Arithmetic.java
@@ -8,13 +8,30 @@ public class Arithmetic {
         // System.out.println(c);
         // System.out.println(r);
 
-        float a=14.3f, b=3.2f;
+        // float a=14.3f, b=3.2f;
 
         // float c=(float)a/b;
         // System.out.println(c);
 
-        float r=a%b;
-        System.out.println(r);        
+        // float r=a%b;
+        // System.out.println(r);  
+        
+        // byte a=10;
+        // short b=15;
+
+        // int c=a+b;
+        // System.out.println(c);
+
+        // float a=12.5f;
+        // int b=123;
+        // double b=123;
+        // double c=a*b;
+        // System.out.println(c);
+
+        char a=40;
+        int b=30;
+        int c=a-b;
+        System.out.println(c);
 
         
     }
---
+++ Triangle.java
@@ -0,0 +1,36 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String args[])
+
+    // { float base, height, area;
+    // System.out.println("Enter Base and Height ");
+
+    // Scanner sc = new Scanner(System.in);
+    // base = sc.nextFloat();
+    // height = sc.nextFloat();
+
+    // area = base * height / 2;
+    // System.out.println("Area of triangle is: " + area);
+    // }
+
+    // Area of triangle using three sides
+
+    {
+        int a, b, c;
+        float s;
+        double area;
+        System.out.println("Enter three sides of the triangle ");
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        s = (a + b + c)/2f;
+        area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        System.out.println("Area of triangle is: " + area);
+
+    }
+
+}
