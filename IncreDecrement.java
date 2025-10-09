148f9c33438772c97475f2ffe8908e271f600361
madhvikrana <149559023+madhvikrana@users.noreply.github.com>
2025-10-09 09:36:45 -0400

    commit16

---
+++ IncreDecrement.java
@@ -0,0 +1,25 @@
+public class IncreDecrement {
+
+    public static void main(String[] args)
+    {
+        // int x=5, y;
+        // y=x++;
+        int x=5, y=4, z;
+        z=2 * x++ +3 * ++x; 
+        System.out.println(z);
+
+        float a=3.5f;
+        a++;
+        System.out.println(a);
+
+        char b='A';
+        b++;
+        System.out.println(b);
+
+        byte c=5;
+        c++;
+        System.out.println(c);
+
+    }
+    
+}
